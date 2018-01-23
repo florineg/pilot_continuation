0dbfde3f56d7c38f99030da5d85188ef0708b009
florineg <florinegroenen@hotmail.com>
2018-01-23 15:12:45 +0100

    update2 3 uur

--- src/seminar/MaxModel.java
+++ src/seminar/MaxModel.java
@@ -1,6 +1,5 @@
 package seminar;
 
-import java.awt.Event;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- src/seminar/World.java
+++ src/seminar/World.java
@@ -73,10 +73,6 @@ public class World {
 		int[] resultsObjective = new int[13];
 		for (int d = 1; d<=12; d++) {
 			MaxModel m = new MaxModel(pilots, events, planes, 20, 4.5);
-			// add additional constraints
-//			m.initAssignTasks(); 
-//			m.initHolidays(); 
-//			m.initOfficeTask(); 
 			
 			if(m.solve()) {
 				m.printSolution();
@@ -85,6 +81,7 @@ public class World {
 				resultsObjective[d-1] = m.getObjectiveX(); 
 			}
 		}
+		
 		MaxModel m = new MaxModel(pilots, events, planes, 14, 1.2);
 		
 		if(m.solve()) {
