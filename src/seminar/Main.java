cded3918aad2355af590259a35fad441338cd25b
Friso H. Kingma <fhkingma@gmail.com>
2018-01-24 12:35:13 +0100

    Commit development

--- src/seminar/Main.java
+++ src/seminar/Main.java
@@ -1,12 +1,13 @@
 package seminar;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import ilog.concert.IloException;
 
 public class Main {
 
-	public static void main(String [ ] args) throws IloException, objectNotFoundException {
+	public static void main(String [ ] args) throws IloException, objectNotFoundException, IOException {
 		World thisWorld = new World();  
 		thisWorld.runMaxModel();
 	}
